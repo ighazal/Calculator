26bbb13867690e2c210ff91a831a06237eef5994
Ghazal <taranehjj489@gmail.com>
2023-05-27 16:35:58 -0700

    First step of project

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
-/*var x = document.getElementById("monster").value;
+function mosavi(){
+var x = document.getElementById("monster").value;
 var numbers = [];
 var amalgars = ["-","+","*","/"];
-if (var i = 0; i <= x.length; i++){
-     if (amalgars.includes(x[i]) == false){
-          numbers.push(x.split());
-     }
-}*/
+var number = "";
+var operator = "";
+var result = 0;    
+    for (var i = 0; i < x.length; i++){
+        if (amalgars.includes(x[i]) == false){
+            number += x[i];
+        }
+        else if (amalgars.includes(x[i])){
+            if (operator!==""){
+                switch(operator){
+                    case "+":
+                        result += parseInt(number); 
+                        break;
+                    case "-":
+                        result -= parseInt(number);
+                        break;
+                    case "*":
+                        result *= parseInt(number);
+                        break;
+                    case "/":
+                        result /= parseInt(number);
+                        break;            
+                } 
+                number = "";
+                operator = x[i];
+            }
+            else {
+                result = parseInt(number);
+                number = "";
+                operator = x[i];
+            }
+        }
+        if (i == x.length-1){
+            switch(operator){
+                case "+":
+                    result += parseInt(number); 
+                    break;
+                case "-":
+                    result -= parseInt(number);
+                    break;
+                case "*":
+                    result *= parseInt(number);
+                    break;
+                case "/":
+                    result /= parseInt(number);
+                    break;            
+            } 
+            number = "";
+        }
+    }
+document.getElementById("result").value = result;
+}
 function display(value) {
         document.getElementById("monster").value += value;
 }
